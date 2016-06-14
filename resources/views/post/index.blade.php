a039bceb1d1f4a543521dde698ccb0f30c26cd8f
deminua <dstaranenko@gmail.com>
2016-06-14 18:49:31 +0300

    Update scss, add animation

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,15 +3,49 @@
 @section('content')
 
 <div class="container">
+
+
     <div class="row">
 
+
         <div class="col-sm-12">
-                @foreach($posts as $post)
-                  <div class="col-xs-12 col-sm-6 col-md-4 text-center">
+
+          <h1>Новости</h1>
+          <p>Все события и новости нашей компании...</p>
+
+@foreach ($posts as $k => $post)
+
+    @if ($k % 2 == 0)
+    <div class="news-row row even text-left">
+                  <div class="col-xs-12 col-md-3">
+                    <a href="{!! route('post.show', ['post' => $post->id]) !!}" class="thumbnail">
+                      <img src="{!! $post->avatar !!}" alt="{!! $post->title !!}">
+                    </a>
+
+                  </div>
+                  <div class="col-xs-12 col-md-9">
                         <h2><a href="{!! route('post.show', ['post' => $post->id]) !!}">{!! $post->title !!}</a></h2>
+                        <time>{!! $post->created_at !!}</time>
                         {!! $post->text !!}
                   </div>
-                @endforeach
+                  </div>
+    @else
+    <div class="news-row row odd text-right">
+                  <div class="col-xs-12 col-md-3 col-md-push-9">
+                    <a href="{!! route('post.show', ['post' => $post->id]) !!}" class="thumbnail">
+                      <img src="{!! $post->avatar !!}" alt="{!! $post->title !!}">
+                    </a>
+                  </div>
+                  <div class="col-xs-12 col-md-9 col-md-pull-3">
+                        <h2><a href="{!! route('post.show', ['post' => $post->id]) !!}">{!! $post->title !!}</a></h2>
+                        <time>{!! $post->created_at !!}</time>
+                        {!! $post->text !!}
+                  </div>
+    </div>
+    @endif 
+@endforeach
+
+
         </div>
   </div>
 </div>
